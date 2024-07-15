cc8416dec15abcad5bcdd056b33bebfa70bb9c61
wuwen03 <1056374449@qq.com>
2024-07-15 17:17:27 +0800

    fix bug translate

--- pages/trans/trans.js
+++ pages/trans/trans.js
@@ -12,23 +12,30 @@ Page({
       if (originalText.trim() === '') return;
       console.log('originalText');
       try {
+        let that = this
         const response = await wx.request({
-          url: 'https://47.113.205.144:5000/ecs/translate',
+          url: 'http://47.113.205.144:5000/translate/',
           method: 'POST',
           data: { 
-            "src_language":getApp().globalData.chosen_language,
+            "src_language":"英文",
             "dst_language":"CN",
             "content":originalText },
-          header: {
-            'content-type': 'application/json'
+          // header: {
+          //   'content-type': 'application/json'
+          // }
+          success(res) {
+            console.log(res)
+            that.setData({originalText: '',
+                          errorMessage: '',
+                          translatedText: res.data.result })
           }
         });
   
-        if (response.statusCode === 200 && response.data && response.data.result) {
-          this.setData({ translatedText: response.data.result });
-        } else {
-          this.setData({ errorMessage: response.data.message || '翻译失败，请重试。' });
-        }
+        // if (response.statusCode === 200 && response.data && response.data.result) {
+        //   this.setData({ translatedText: response.data.result });
+        // } else {
+        //   this.setData({ errorMessage: response.data.message || '翻译失败，请重试。' });
+        // }
       } catch (error) {
         this.setData({ errorMessage: '网络请求失败，请检查网络连接。' });
         console.error('Translation failed:', error);
