88b6f73ef9da67eaf45a4722725454a29058984c
lidu <10225501448@ecnu.edu.cn>
2024-07-16 13:15:49 +0800

    modify ocr: upload filepath

--- be/view/ocr.py
+++ be/view/ocr.py
@@ -1,17 +1,33 @@
-#翻译
-
+import logging
+import os
+import uuid
 from flask import Blueprint,request,jsonify
 from be.model.translate import Translate
 from be.model.ocr import Ocr
 
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(cur_dir, 'uploads_tmp/')
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+    
 bp_ocr = Blueprint("ocr",__name__,url_prefix="/ocr")
 
-@bp_ocr.route("/",methods=['POST'])
+@bp_ocr.route("/upload",methods=['POST'])
 def ocr():
-    # dst_language = request.json.get("dst_language")
-    picture = request.json.get("picture")
-
-    ocr = Ocr(picture)
-    code,message,content = ocr.do_ocr()
+    # picture = request.json.get("picture")
+    if 'file' not in request.files:
+        return jsonify({"message":"no file part","result":""}), 520
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"message":"no selected file","result":""}), 521
+    if file:
+        # 使用 uuid4 生成唯一的文件名
+        unique_filename = str(uuid.uuid4()) + "_" + file.filename
+        filepath = os.path.join(UPLOAD_FOLDER, unique_filename)
+        file.save(filepath)
+        ocr = Ocr(filepath)
+        code,message,content = ocr.do_ocr()
+        os.remove(filepath)
 
     return jsonify({"message":message,"result":content}),code
+
--- fe/access/ocr.py
+++ fe/access/ocr.py
@@ -6,8 +6,9 @@ class Ocr:
     def __init__(self, url_prefix):
         self.url_prefix = url_prefix
 
-    def do_ocr(self, picture: str) -> tuple[(int, str)]:
-        json = {"picture": picture}
-        url = urljoin(self.url_prefix, "ocr")
-        r = requests.post(url, json=json)
-        return r.status_code, r.json().get("result")
+    def do_ocr(self, pic_path: str) -> tuple[(int, str)]:
+        with open(pic_path, 'rb') as f:
+            files = {'file': f}
+            url = urljoin(self.url_prefix, "ocr/upload")
+            r = requests.post(url, files=files)
+            return r.status_code, r.json().get("result")
